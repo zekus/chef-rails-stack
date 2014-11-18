2b769b6c3aaa1eb79e2ff5f917c4de5d123d4e4a
antonio lorusso <zekus.one@gmail.com>
2014-11-18 11:36:00 +0100

    install server essential packages
    change the timezone and create the swap

--- recipes/general.rb
+++ recipes/general.rb
@@ -1,29 +1,44 @@
 # general components for server
 
+# Configure the proper timezone
+execute 'configure timezone' do
+  command 'echo "America/Los_Angeles" > /etc/timezone; dpkg-reconfigure --frontend noninteractive tzdata'
+end
+
+# fix the locale
 execute "locale-gen" do
   command "locale-gen en_US.UTF-8"
 end
 
+# create some swapping space
+swap_file '/mnt/swap' do
+  size 1024
+end
+
+# recipes
 include_recipe "apt"
 include_recipe "nodejs"
 include_recipe "memcached"
-
 include_recipe "nginx"
-
-service "nginx" do
-  supports :reload => true
-end
-
 include_recipe "rbenv"
 
+# base packages
+package 'htop'
+package 'git-core'
+package 'ssl-cert'
+
 # install image processing libraries
-%w{imagemagick libmagickcore-dev libmagickwand-dev advancecomp gifsicle jpegoptim libjpeg-progs optipng pngcrush}.each do |pkg|
+%w{imagemagick libmagick++-dev libmagickcore-dev libmagickwand-dev advancecomp gifsicle jpegoptim libjpeg-progs optipng pngcrush}.each do |pkg|
   package pkg
 end
 
 # install monit
 package "monit" do
   action :install
+end
+
+service "nginx" do
+  supports :reload => true
 end
 
 # some system magick
